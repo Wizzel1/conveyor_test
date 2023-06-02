6085c8a8d43ef08e38006d430d06ec3b1c54325c
Wizzel1 <r_christian@gmx.de>
2023-06-02 11:45:00 +0200

    Add changes

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,13 @@ class MainApp extends StatelessWidget {
         body: Center(
           child: Column(
             children: const [
-              Text('Hello World! 3'),
-              Text('Hello World! 3'),
-              Text('Hello World! 3'),
-              Text('Hello World! 3'),
-              Text('Hello World! 3'),
-              Text('Hello World! 3'),
-              Text('Hello World! 3'),
+              Text('Hello World! 4'),
+              Text('Hello World! 4'),
+              Text('Hello World! 4'),
+              Text('Hello World! 4'),
+              Text('Hello World! 4'),
+              Text('Hello World! 4'),
+              Text('Hello World! 4'),
             ],
           ),
         ),
